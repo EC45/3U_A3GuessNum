4ce34462da42d2277df8033ab54de557dad434c6
EC45 <90916071+EC45@users.noreply.github.com>
2021-11-24 17:35:40 -0500

    Revert "Update A3GuessNum.java"

--- A3GuessNum.java
+++ A3GuessNum.java
@@ -1,44 +1,55 @@
+/*
+*Name:
+Eric
+*Date:
+11/23/2021
+*Desription:
+*/
 
 import java.util.*;
 
-public class GuessGame{
+public class A3GuessNum{
+  
+  //int min and max
+  // int min = 0;
+  // int max = 100;
+  // Scanner input = new Scanner(System.in);
+  // Random numGenerator = new Random();
+  //main method
+  public static void main(String[] arg){
+    // Make a scanner and Random Object.
+   Scanner input = new Scanner(System.in);
+   Random numGenerator = new Random();
+   
+  
+    // Generate a random number from 0-5.
+    int answer = numGenerator.nextInt(100) + 1;
+    int numberofTries = 5; // default guess num.
+    int guess;
+    System.out.println("Welcome to Guessing a number! \n");
+    System.out.println("Guess a number, you have 5 guesses!!");
+    System.out.println("If you lose... pfft game over!");
+    System.out.print("Your number: ");
+    guess = input.nextInt();
+    numberofTries--;
+   //If the number is high or low, you lose a guess
+    if (guess == answer){
+    } else if (guess < answer && guess != numberofTries -1){
+      System.out.println("Your number is too low");
+      System.out.println("Your current guess: " + numberofTries);
+    }else if(guess > answer && guess != numberofTries -1){
+      System.out.println("Your number is too high");
+      System.out.println("Your current guess: " + numberofTries);
+    }
 
-    //int min and max
-    // int min = 0;
-    // int max = 100;
-    // Scanner input = new Scanner(System.in);
-    // Random numGenerator = new Random();
-    //main method
-    public static void main(String[] arg){
-        // Make a scanner and Random Object.
-        Scanner sc = new Scanner(System.in);
-        Random numGenerator = new Random();
-
-
-        // Generate a random number from 0-5.
-        int answer = numGenerator.nextInt(100);
-        int live = 3; // default guess num.
-        int guess;
-        System.out.println("Welcome to Guessing a number! \n");
-        System.out.println("Guess a number, you have 5 guesses!!");
-        System.out.println("If you lose... pfft game over!");
-        System.out.print("Your number: ");
-
-        while(sc.hasNextInt()){
-            guess = sc.nextInt();
-            if (guess == answer && live > 0){
-                System.out.println("Correct !!");
-            } else if (guess < answer && live > 0){
-                live--;
-                System.out.println("Your current guess : " + guess);
-                System.out.println("Your number is too low");
-                System.out.println("Your current live: " + live);
-            }else if(guess > answer && live > 0){
-                live--;
-                System.out.println("Your current guess : " + guess);
-                System.out.println("Your number is too high");
-                System.out.println("Your current live: " + live);
-            }
-        }
-    }// close main
+    // if (answer == guess){
+    //   System.out.println("Congrats!");
+    //     break;
+      
+    // } else if (answer > guess && guess != numberofTries -1){
+    //     System.out.println("The number is " + "greater than" + guess);
+    // }
+    
+      
+  }// close main
 }
