39fbde32d1933273125cb464749d7daa3c76f049
Eric Cho <90916071+EC45@users.noreply.github.com>
2021-12-07 01:00:52 +0000

    Adding losing code

--- A3GuessNum.java
+++ A3GuessNum.java
@@ -10,6 +10,7 @@ import java.util.*;
 
 public class A3GuessNum{
   
+  static int in, state;
   //int min and max
   // int min = 0;
   // int max = 100;
@@ -31,7 +32,8 @@ public class A3GuessNum{
     System.out.println("Welcome to Guessing a number! \n");
     System.out.println("Guess a number, you have 5 guesses!!");
     System.out.println("If you lose... pfft game over!");
-
+    System.out.print("Your number: ");
+    
     while(sc.hasNextInt()) {
       guess = sc.nextInt();
       if (guess == answer && lives > 0){
@@ -41,17 +43,22 @@ public class A3GuessNum{
           System.out.println("Your current guess : " + guess);
           System.out.println("Your number is too low");
           System.out.println("Your current live: " + lives);
+          System.out.print("Please enter a new number: ");
       } else if (guess > answer && lives > 0){
           lives--;
           System.out.println("Your current guess : " + guess);
           System.out.println("Your number is too high");
           System.out.println("Your current live: " + lives);
+          System.out.print("Please enter a new number: ");
+      }
+      if(lives == 0 && state == 1 && in != num){
+        System.out.println("\nYou Lose!");
+            System.out.println("The number is: " + num);
+            System.out.println("Do you want to continue? (Enter \"Yes\" or \"No\")");
+            state = 0;
+               
       }
     }
-
-    
-    
-
     
 
 
@@ -63,8 +70,6 @@ public class A3GuessNum{
     //     System.out.println("The number is " + "greater than" + guess);
     // }
     
-   // close main
-   
-   }
+      
+  }// close main
 }
-
